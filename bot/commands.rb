e18604ae9d94193d6e38562648f39efdf022bfa0
mr.exz <root@exzec.ru>
2020-01-12 17:44:30 +0300

    add some optimisation

--- bot/commands.rb
+++ bot/commands.rb
@@ -231,11 +231,11 @@ class Commands
       if !dutys.opsgenie_schedule_name.nil?
         notification = NotifyOpsgenie.new
         json_response = JSON.parse(notification.GetOnCall(schedule_name: dutys.opsgenie_schedule_name).body)
-        user = User.where(contacts: json_response['data']['onCallRecipients'][0]).first
-        set_user_on_duty(data: data, client: client, user: user)
+        users = User.where(contacts: json_response['data']['onCallRecipients'][0]).first
+        set_user_on_duty(data: data, client: client, user: users) if users.slack_user_id != duty.user.slack_user_id
       end
 
-      #return if user.slack_user_id == duty.user.slack_user_id
+      return if data.user == duty.user.slack_user_id
 
       if time.utc.strftime('%H%M%S%N') < duty.duty_from.utc.strftime('%H%M%S%N') or time.utc.strftime('%H%M%S%N') > duty.duty_to.utc.strftime('%H%M%S%N')
         from_time = (duty.duty_from.utc + user.tz_offset).strftime('%H:%M').to_s

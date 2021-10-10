ffe95d2fd1a71af6de13bcd7cbcd979969926f9e
Timofey Kapranov <tkapranov@gmail.com>
2021-10-10 22:20:43 +0300

    added actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :messages
   resources :users
   resources :answers
+  resources :actions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

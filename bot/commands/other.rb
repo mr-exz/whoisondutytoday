f7b3210faf4bf0cf81b38d54264d7684217d9a8e
mr.exz <root@exzec.ru>
2023-10-09 14:44:36 +0300

    fix

--- bot/commands/other.rb
+++ bot/commands/other.rb
@@ -111,7 +111,7 @@ module WhoIsOnDutyTodaySlackBotModule
           reason = I18n.t("reply.reason.non-working-day.text")
         end
 
-        if !duty.user.status.nil? && !duty.user.status == "work"
+        if !duty.user.status.nil? || !duty.user.status == "work"
           reason = I18n.t("commands.user.status.enabled.text", status: duty.user.status)
         end
 

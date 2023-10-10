11cd091838b777f7f649056dc87f21985dd2f632
mr.exz <root@exzec.ru>
2023-10-10 09:37:37 +0300

    add report for 9 weeks

--- bot/commands/channel_labels_statistic.rb
+++ bot/commands/channel_labels_statistic.rb
@@ -2,16 +2,21 @@ module WhoIsOnDutyTodaySlackBotModule
   module Commands
     class ChannelLabelsStatistic
       def self.call(client:, data:, match:)
-        start_date = (Time.now - 7.days).beginning_of_week
-        end_date = (Time.now - 7.days).end_of_week
-        m = SlackThread.joins(:labels).where(channel_id: data.channel).where('thread_ts BETWEEN ? AND ?', start_date.to_time.to_i, end_date.to_time.to_i).group(:label)
+        message = ""
+        (0..8).to_a.each do |x|;
+          start_date = (Time.now - x.week).beginning_of_week
+          end_date = (Time.now - x.week).end_of_week
+          m = SlackThread.joins(:labels).where(channel_id: data.channel).where('thread_ts BETWEEN ? AND ?', start_date.to_time.to_i, end_date.to_time.to_i).group(:label)
+          message.concat(I18n.t("commands.thread.statistic",start_date:start_date.strftime("%d.%m.%Y %H:%M"),end_date:end_date.strftime("%d.%m.%Y %H:%M"),labels:m.count.to_s))
+        end
 
         client.web_client.chat_postMessage(
           channel: data.channel,
-          text: I18n.t("commands.thread.statistic",start_date:start_date.strftime("%d.%m.%Y %H:%M"),end_date:end_date.strftime("%d.%m.%Y %H:%M"),labels:m.count.to_s),
+          text: message,
           thread_ts: data.thread_ts || data.ts,
           as_user: true
         )
+
       end
     end
   end

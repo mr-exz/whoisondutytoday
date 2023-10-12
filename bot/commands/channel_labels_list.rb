139f09ef04058663be126e0d8c11b66865bf5628
mr.exz <root@exzec.ru>
2023-10-12 23:41:21 +0300

    fix to s

--- bot/commands/channel_labels_list.rb
+++ bot/commands/channel_labels_list.rb
@@ -5,7 +5,7 @@ module WhoIsOnDutyTodaySlackBotModule
         m = SlackThread.joins(:labels).where(channel_id: data.channel).group(:label).count.keys
         client.web_client.chat_postMessage(
           channel: data.channel,
-          text: m,
+          text: m.to_s,
           thread_ts: data.thread_ts || data.ts,
           as_user: true
         )

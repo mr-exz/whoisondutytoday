4d3059bdee69e5ef39665fcdf5080f0db8ebf662
mr.exz <root@exzec.ru>
2023-11-02 00:04:11 +0300

    fix outptu

--- bot/commands/channel_labels_statistic.rb
+++ bot/commands/channel_labels_statistic.rb
@@ -7,7 +7,7 @@ module WhoIsOnDutyTodaySlackBotModule
           start_date = (Time.now - x.week).beginning_of_week
           end_date = (Time.now - x.week).end_of_week
           m = SlackThread.joins(:labels).where(channel_id: data.channel).where('thread_ts BETWEEN ? AND ?', start_date.to_time.to_i, end_date.to_time.to_i).group(:label)
-          message.concat(I18n.t("commands.thread.statistic",start_date:start_date.strftime("%d.%m.%Y %H:%M"),end_date:end_date.strftime("%d.%m.%Y %H:%M"),labels:m.count.to_s))
+          message.concat(I18n.t("commands.thread.statistic",start_date:start_date.strftime("%d.%m.%Y %H:%M"),end_date:end_date.strftime("%d.%m.%Y %H:%M"),labels:JSON.pretty_generate(m.count).gsub(":", " =>")))
         end
 
         client.web_client.chat_postMessage(

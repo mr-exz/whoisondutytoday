ed2a82765189dd7b46295f41891e8f13febb345e
mr-exz <root@exzec.ru>
2024-12-02 12:00:42 +0300

    added new action

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Changelog
+## 0.12.0
+### Features
+- Added new action `git commits` - it will return last 10 user commits
+
 ## 0.11.0
 ### Features
 - Added Bitbucket Datacenter commits report feature
---
+++ bot/commands/git_commits.rb
@@ -0,0 +1,29 @@
+module WhoIsOnDutyTodaySlackBotModule
+  module Commands
+    class UserCommits
+      def self.call(client:, data:, match:)
+        message_processor = MessageProcessor.new
+        user_name = match["expression"][/<@(.+)>/, 1]
+        message_processor.collectUserInfoBySlackUserId(user_name)
+        user = User.where(slack_user_id: user_name).first
+        return unless user
+        commits = BitbucketCommit.where('LOWER(author) = ?', user.contacts.downcase).order(date: :desc).limit(10)
+        created_at = commits.first.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        commit_messages = commits.map { |commit|
+          commit_url = generate_commit_url(commit)
+          "*#{commit.date.strftime('%Y-%m-%d')}*: <#{commit_url}|#{commit.message}>"
+        }.join("\n")
+
+        client.web_client.chat_postMessage(
+          channel: data.channel,
+          text: "Last 10 commits by #{user.name} synced at: #{created_at}:\n#{commit_messages}",
+          thread_ts: data.thread_ts || data.ts,
+          as_user: true
+        )
+      end
+      def self.generate_commit_url(commit)
+        "#{ENV['BITBUCKET_URL']}/projects/#{commit.project_key}/repos/#{commit.repo_slug}/commits/#{commit.commit_id}"
+      end
+    end
+  end
+end

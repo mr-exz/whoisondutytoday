74a045c87576a11b16be4fe46290dcac450cd007
mr-exz <root@exzec.ru>
2024-12-21 01:09:34 +0300

    fix

--- bot/commands/channel_tag_reporter_in_thread_disable.rb
+++ bot/commands/channel_tag_reporter_in_thread_disable.rb
@@ -1,6 +1,9 @@
 module WhoIsOnDutyTodaySlackBotModule
   module Commands
     class ChannelTagReporterInThreadDisable < SlackRubyBot::Commands::Base
+
+      DESCRIPTION = 'Bot will not tag the reporter in the thread.'.freeze
+      EXAMPLE = 'Usage: `channel tag reporter in thread disable`'.freeze
       def self.call(client:, data:)
         channel = Channel.find_or_initialize_by(slack_channel_id: data.channel)
         channel.update(tag_reporter_enabled: false)
--- bot/commands/channel_tag_reporter_in_thread_enable.rb
+++ bot/commands/channel_tag_reporter_in_thread_enable.rb
@@ -1,6 +1,9 @@
 module WhoIsOnDutyTodaySlackBotModule
   module Commands
     class ChannelTagReporterInThreadEnable < SlackRubyBot::Commands::Base
+
+      DESCRIPTION = 'Bot will tag the reporter in the thread.'.freeze
+      EXAMPLE = 'Usage: `channel tag reporter in thread enable`'.freeze
       def self.call(client:, data:)
         channel = Channel.find_or_initialize_by(slack_channel_id: data.channel)
         channel.update(tag_reporter_enabled: true)

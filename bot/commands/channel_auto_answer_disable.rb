6ceb2fc1a3f415124e7b2150e2dd27929bf6c97c
mr-exz <root@exzec.ru>
2024-12-21 01:07:16 +0300

    fix

--- bot/commands/channel_auto_answer_disable.rb
+++ bot/commands/channel_auto_answer_disable.rb
@@ -1,6 +1,9 @@
 module WhoIsOnDutyTodaySlackBotModule
   module Commands
     class ChannelAutoAnswerDisable < SlackRubyBot::Commands::Base
+
+      DESCRIPTION = 'Bot stop answer on any message in channel at working time'.freeze
+      EXAMPLE = "Usage: `channel auto answer disable`".freeze
       def self.call(client:, data:)
         channel = Channel.find_or_initialize_by(slack_channel_id: data.channel)
         channel.update(auto_answer_enabled: false)
